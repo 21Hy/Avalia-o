87af07bf3a40679188608df59cd5175e8ba161d2
Luís Raminhas <119435119+21Hy@users.noreply.github.com>
2022-12-04 17:49:56 +0000

    Update functions.py

--- lab4_1/functions.py
+++ lab4_1/functions.py
@@ -2,31 +2,24 @@
 def mesmo_tamanho(lista1, lista2):
     if len(lista1) == len(lista2):
         return True
-# parece estar certo.
-
 
 # exercicio 1, b)
 def valores_repetidos(lista):
     lista_retorno = []
     nova_lista = []
-
     for elemento in lista:
         if elemento not in lista_retorno:
             lista_retorno.append(elemento)
         else: 
             nova_lista.append(elemento)
-
     return nova_lista
 
-
 # exercicio 1, c)
-def soma_do_primeiro_e_ultimo(lista):
-# dúvida, receber em números inteiros ou float em functions???
+def soma_do_primeiro_e_ultimo(lista): # dúvida, receber em números inteiros ou float em functions???
     return lista[0] + lista[len(lista) - 1]
 
 # exercicio 1, d)
-# dúvida, receber em números inteiros ou float em functions???
-def soma_de_duas_listas_com_o_mesmo_indice(lista1, lista2): 
+def soma_de_duas_listas_com_o_mesmo_indice(lista1, lista2): # dúvida, receber em números inteiros ou float em functions???
     if len(lista1) > len(lista2):
         retorno = []
         for i in range(len(lista2)):
@@ -37,9 +30,6 @@ def soma_de_duas_listas_com_o_mesmo_indice(lista1, lista2):
             retorno.append(lista1[i] + lista2[i])
     return retorno
 
-# parece estar certo
-
-
 # exercicio 1, e)
 def retornar_string(lista):
     nova_lista = []
@@ -48,12 +38,9 @@ def retornar_string(lista):
             nova_lista.append(lista[i])
     return nova_lista
 
-
 # exercicio 1, f)
 def media_dos_valores_da_lista(lista):
     return sum(lista)/ len(lista)
-
-
 
 # exercicio 1, g)
 def elementos_arredondados(lista):
@@ -62,7 +49,3 @@ def elementos_arredondados(lista):
         arredondamento = int(elemento)
         nova_lista.append(arredondamento)
     return nova_lista
-
-# exercicio 2
-# ideia, requisitos são meras condições, para se poder candidatar-se à vaga é preciso que todas elas sejam
-# True.
