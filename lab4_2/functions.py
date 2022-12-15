3e21c9909d3139eafa2169fe389da43e95da2e9e
Luís Raminhas <119435119+21Hy@users.noreply.github.com>
2022-12-15 10:15:55 +0000

    Update functions.py

--- lab4_2/functions.py
+++ lab4_2/functions.py
@@ -1,20 +1,23 @@
-def jogada(escolha1, escolha2):
+# função de confirmar o input do jogador
+def confirmar_input(input):
+    if ((input == ('pedra') ) or (input == ('papel')) or (input == ('tesoura'))):
+        return True
+    else:
+        return False
 
-    if (escolha1 == 'papel' and escolha2 == 'tesoura' ):
-        pass
-    if (escolha1 == 'papel' and escolha2 == 'pedra' ):
-        pass
-    if (escolha1 == 'pedra' and escolha2 == 'tesoura' ):
-        pass
-    if (escolha1 == 'pedra' and escolha2 == 'papel' ):
-        pass
-    if (escolha1 == 'tesoura' and escolha2 == 'papel' ):
-        pass
-    if (escolha1 == 'tesoura' and escolha2 == 'pedra' ):
-        pass
-    if (escolha1 == 'tesoura' and escolha2 == 'tesoura' ):
-        pass
-    if (escolha1 == 'papel' and escolha2 == 'papel' ):
-        pass
-    if (escolha1 == 'pedra' and escolha2 == 'pedra' ):
-        pass
+# função de decidir a pontuação
+def decidir(lista):
+    # vairiáveis de pontuação
+    x = 0
+    y = 0
+    # condições vituriosas para primeiro jogador
+    if ((lista == ['tesoura', 'papel']) or (lista == ['pedra', 'tesoura']) or (lista == ['papel', 'pedra'])):
+        x += 5
+    # condições vituriosas para segundo jogador
+    if ((lista == ['papel', 'tesoura']) or (lista == ['tesoura', 'pedra']) or (lista == ['pedra', 'papel'])):
+        y += 5
+    # condições de empate
+    if ((lista == ['tesoura', 'tesoura']) or (lista == ['pedra', 'pedra']) or (lista == ['papel', 'papel'])):
+        x += 1
+        y += 1
+    return x, y
