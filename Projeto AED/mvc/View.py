e791b3d25d9cb0aa910cfe42e98adf538651c098
Luís Raminhas <119435119+21Hy@users.noreply.github.com>
2023-05-11 13:48:34 +0100

    Update View.py

--- Projeto AED/mvc/View.py
+++ Projeto AED/mvc/View.py
@@ -1,143 +1,194 @@
 import tkinter as tk
-from model.User import*
+from model.Cliente import*
 from model.LinkedList import*
 from Controller import*
 from model.Despesas import*
+from tkinter import messagebox
 
 class View:
     def __init__(self,master):
         self.master = master
+        self.master.resizable(False, False)
+        self.clientes = LinkedList()
         self.despesas = LinkedList()
         self.controller = Controller(self.master)
-        self.lista_de_objetos_despesas = []
-        self.criar_login()
+        self.preencher_users()
+        self.user_atual = None
+        self.clientes.insert_last(Cliente('Jorge','21','111111111'))
+        self.clientes.insert_last(Cliente('Miguel','21','222222222'))
+        self.clientes.insert_last(Cliente('Ronaldo','31','333333333'))
+        self.login()
 
-    def modificar_geometria(self):
+    def modificar_geometria(self, largura, altura):
         # geometria centrada da janela
-        w=300; h=300
+        w=largura; h=altura
         screen_width = self.master.winfo_screenwidth()
         screen_height = self.master.winfo_screenheight()
         x = (screen_width/2)-(w/2)
         y = (screen_height/2)-(h/2)
         self.master.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-    def criar_login(self):
-        self.modificar_geometria()
-        self.main_label = tk.Label(self.master, text='Login', font='bold', pady=20)
-        # username frame
-        self.un_fr = tk.Frame(self.master)
-        self.un_lb = tk.Label(self.un_fr, text='Username') 
-        self.un_et_str = tk.StringVar()
-        self.un_et = tk.Entry(self.un_fr, width=30, textvariable=self.un_et_str, bg='#b2bdd9') # hex color picker
-        # password frame
-        self.pw_fr = tk.Frame(self.master, pady=10)
-        self.pw_lb = tk.Label(self.pw_fr, text='Password') 
-        self.pw_et_str = tk.StringVar()
-        self.pw_et = tk.Entry(self.pw_fr, width=30,textvariable=self.pw_et_str, show='*', bg='#b2bdd9')
-        # nif frame
-        self.nif_fr = tk.Frame(self.master, pady=10)
-        self.nif_lb = tk.Label(self.pw_fr, text='Nif') 
-        self.nif_et_str = tk.StringVar()
-        self.nif_et = tk.Entry(self.pw_fr, width=30,textvariable= self.nif_et_str, bg='#b2bdd9') 
-        # Entry frame
-        self.et_frame = tk.Frame(self.master)
-        self.et_bt_in = tk.Button(self.et_frame, text="Enter", width=25, bg='#c4c2bc')
-        self.et_bt_in.config(command= lambda: self.controller.login_enter_click(self.un_et_str,self.pw_et_str,self.nif_et_str))
-        self.et_lb = tk.Label(self.et_frame)
-        self.et_bt_sp = tk.Button(self.et_frame, text="Don´t have an acount yet? Sign up here", font=('','7'), bg='#c4c2bc',width=29)
-        self.et_bt_sp.config(command = self.sign_up_window)
-        # packs
-        self.main_label.pack()
-        self.un_fr.pack()
-        self.un_lb.pack()
-        self.un_et.pack()
-        self.pw_fr.pack()
-        self.pw_lb.pack()
-        self.pw_et.pack()
-        self.nif_fr.pack()
-        self.nif_lb.pack()
-        self.nif_et.pack()
-        self.et_frame.pack()
-        self.et_bt_in.pack()
-        self.et_lb.pack()
-        self.et_bt_sp.pack()
+    def login(self):
+        self.modificar_geometria(250,400)
+        self.frame = tk.Frame(self.master)
+        self.lb_login = tk.Label(self.frame, text='Login', font=('Arial', 20))
+        # username 
+        self.un_lb = tk.Label(self.frame, text='Username',font=('Arial', 14)) 
+        self.un_et = tk.Entry(self.frame, font=('Arial', 14), bg='#b2bdd9') 
+        # password 
+        self.pw_lb = tk.Label(self.frame, text='Password',font=('Arial', 14)) 
+        self.pw_et = tk.Entry(self.frame, font=('Arial', 14), show='*', bg='#b2bdd9')
+        # nif 
+        self.nif_lb = tk.Label(self.frame, text='Nif',font=('Arial', 14)) 
+        self.nif_et = tk.Entry(self.frame, font=('Arial', 14), bg='#b2bdd9') 
+        # botões
+        self.bt_login = tk.Button(self.frame, text="Enter",font=('Arial', 14), bg='#c4c2bc')
+        self.bt_login.config(command = self.verificar_login)
+        self.bt_sign_up = tk.Button(self.frame, text="Sign-up", font=('Arial', 14), bg='#c4c2bc')
+        self.bt_sign_up.config(command = self.sign_up)
+        self.login_packs()
 
-    def sign_up_window(self):
+    def sign_up(self):
         self.master.destroy()
         self.master = tk.Tk()
-        self.modificar_geometria()
-        self.main_label_2 = tk.Label(self.master, text='Sign-up', font='bold', pady=20)
-        # username frame
-        self.un_fr = tk.Frame(self.master)
-        self.un_lb = tk.Label(self.un_fr, text='Username') 
-        self.un_et_str = tk.StringVar()
-        self.un_et = tk.Entry(self.un_fr, width=30, textvariable=self.un_et_str, bg='#b2bdd9') # hex color picker
-        # password frame
-        self.pw_fr = tk.Frame(self.master, pady=10)
-        self.pw_lb = tk.Label(self.pw_fr, text='Password') 
-        self.pw_et_str = tk.StringVar()
-        self.pw_et = tk.Entry(self.pw_fr, width=30,textvariable=self.pw_et_str, show='*', bg='#b2bdd9')
-        # password frame
-        self.pw_lb_2 = tk.Label(self.pw_fr, text='Confirm password') 
-        self.pw_et_str_2 = tk.StringVar()
-        self.pw_et_2 = tk.Entry(self.pw_fr, width=30,textvariable=self.pw_et_str_2, show='*', bg='#b2bdd9')
-        # nif frame
-        self.nif_lb_2 = tk.Label(self.pw_fr, text='Nif') 
-        self.nif_et_str_2 = tk.StringVar()
-        self.nif_et_2 = tk.Entry(self.pw_fr, width=30,textvariable=self.nif_et_str_2, bg='#b2bdd9')
-        # Entry frame
-        self.et_fr = tk.Frame(self.master)
-        self.et_bt_in = tk.Button(self.et_fr, text="Enter", width=25, bg='#c4c2bc')
-        self.et_bt_in.config(command=lambda:self.controller.sign_up_click(self.un_et_str,self.pw_et_str, self.pw_et_str_2, self.nif_et_str_2))
-        # packs
-        self.main_label_2.pack()
-        self.un_fr.pack()
+        self.master.resizable(False, False)
+        self.modificar_geometria(250,400)
+        self.frame = tk.Frame(self.master)
+        self.lb_sign_up = tk.Label(self.frame, text='Sign-up', font=('Arial', 20))
+        # username 
+        self.un_lb = tk.Label(self.frame, text='Username',font=('Arial', 14)) 
+        self.un_et = tk.Entry(self.frame, font=('Arial', 14),  bg='#b2bdd9') 
+        # password 
+        self.pw_lb = tk.Label(self.frame, text='Password',font=('Arial', 14)) 
+        self.pw_et = tk.Entry(self.frame, font=('Arial', 14), show='*', bg='#b2bdd9')
+        # confirm password
+        self.pw_lb_2 = tk.Label(self.frame, text='Confirm password',font=('Arial', 14)) 
+        self.pw_et_2 = tk.Entry(self.frame, font=('Arial', 14), show='*', bg='#b2bdd9')
+        # nif 
+        self.nif_lb = tk.Label(self.frame, text='Nif',font=('Arial', 14)) 
+        self.nif_et = tk.Entry(self.frame, font=('Arial', 14), bg='#b2bdd9')
+        # Botão
+        self.bt = tk.Button(self.frame, text="Enter", font=('Arial', 14), bg='#c4c2bc')
+        self.bt.config(command= self.verificar_registo)
+        self.sign_up_packs()
+        self.master.mainloop()
+
+    def gestor_despesas(self):
+        self.master.destroy()
+        self.master = tk.Tk()
+        self.master.resizable(False, False)
+        self.modificar_geometria(800,500)
+        # ...
+        # ...
+        # ...
+        # ...
+        # ...
+        self.master.mainloop()
+
+    def verificar_login(self):
+        if (
+            (self.clientes.find_nif(self.nif_et.get()) !=-1) and
+            (self.clientes.get(
+                    (self.clientes.find_nif(self.nif_et.get()))
+                ).get_username() == self.un_et.get()) and
+            (self.clientes.get(
+                    (self.clientes.find_nif(self.nif_et.get()))
+                ).get_password() == self.pw_et.get())
+        ):
+            self.user_atual = self.nif_et.get()
+            self.preencher_despesas()
+            self.gestor_despesas()
+        else:
+            self.produzir_msg_erro('Login', 'Falha no login', 0)
+
+    def verificar_registo(self):
+        if ( 
+            (self.clientes.find_nif(self.nif_et.get()) != -1) or
+            (len(self.nif_et.get()) != 9)
+        ):
+            self.produzir_msg_erro('Registo','Falha no registo',0)
+        else:
+            self.clientes.insert_last(
+                Cliente(self.un_et.get(), self.pw_et.get(), self.nif_et.get())
+            )
+            self.user_atual = self.nif_et.get()
+            self.preencher_despesas()
+            self.gestor_despesas()
+
+
+    def produzir_msg_erro(self, titulo, texto, valor):
+        if valor == 0:
+            return messagebox.showerror(titulo, texto)
+        if valor == 1:
+            return messagebox.showinfo(titulo, texto)
+            
+
+    def login_packs(self):
+        self.frame.pack()
+        self.lb_login.pack(pady=40)
         self.un_lb.pack()
         self.un_et.pack()
-        self.pw_fr.pack()
+        self.pw_lb.pack()
+        self.pw_et.pack()
+        self.nif_lb.pack()
+        self.nif_et.pack()
+        self.bt_login.pack(pady=5, ipadx=80, ipady=1)
+        self.bt_sign_up.pack(pady=5, ipadx=70, ipady=1)
+    
+    def sign_up_packs(self):
+        self.frame.pack()
+        self.lb_sign_up.pack(pady=40)
+        self.un_lb.pack()
+        self.un_et.pack()
         self.pw_lb.pack()
         self.pw_et.pack()
         self.pw_lb_2.pack()
         self.pw_et_2.pack()
-        self.nif_lb_2.pack()
-        self.nif_et_2.pack()
-        self.et_fr.pack()
-        self.et_bt_in.pack()
-        # end-loop
-        self.master.mainloop()
+        self.nif_lb.pack()
+        self.nif_et.pack()
+        self.bt.pack(pady=5, ipadx=80, ipady=1)
+        
+    def preencher_users(self):
+        self.controller.data = self.controller.ler_ficheiro_json('data.json')
+        self.controller.users = self.controller.data['Clientes']
+        for elemento in self.controller.users:
+            self.clientes.insert_last(
+                Cliente(elemento['Username'], elemento['Password'], elemento['Nif'])
+            )
+        
+    def preencher_despesas(self):
+        for i in range(len(self.controller.users)):
+            if self.controller.users[i]['Nif'] == self.user_atual:
+                for elmt in self.controller.users[i]['Userdata']:
+                    self.despesas.insert_last(
+                        Despesas( elmt['Categoria'], elmt['Descricao'], elmt['Valor'], elmt['Data'])
+                    )
 
-    indice_de_despesa = 0
-    def inserir_despesa_linkedlist(self, categoria, descricao, valor, data):
-        self.despesas.insert(
-            Despesas(categoria.get(),descricao.get(),valor.get(),data.get()),
-            View.indice_de_despesa)
-        View.indice_de_despesa +=1
+    def guardar(self):
+        for elemento in self.clientes.iterator():
+            self.controller.users.append(
+                {
+                    'Username': elemento.get_username(),
+                    'Password': elemento.password(),
+                    'Nif': elemento.get_nif(),
+                    'Userdata': []
+                }
+            )
 
-    def guardar_linkedlist(self):
-        for i in range(View.indice_de_despesa + 1):
-            self.lista_de_objetos_despesas.append(self.despesas.get(i))
-
-        for elemento in self.lista_de_objetos_despesas:
+        for elemento in self.despesas.iterator():
             self.controller.user_data.append(
                 {
                     'Categoria': elemento.get_categoria(),
-                    'Descrição': elemento.get_descricao(),
+                    'Descricao': elemento.get_descricao(),
                     'Valor': elemento.get_valor(),
                     'Data': elemento.get_data()
                 }
             )
-    def ordenar_despesas_periodo_tempo(self):
-        pass
+        
+        for elemento in self.controller.users:
+            if elemento['Nif'] == self.user_atual:
+                elemento['Userdata'] == self.controller.user_data
+            self.controller.data['Clientes'].append(elemento)
 
-    def ordenar_despesas_categoria(self, categoria):
-        self.lista_de_objetos_despesas_ordenados = []
-        for elemento in self.lista_de_objetos_despesas:
-            if elemento.get_categoria() == categoria:
-                self.lista_de_objetos_despesas_ordenados.append(elemento)
-                
-    def ordenar_despesas_valor_crescente(self):
-        pass
-    def ordenar_despesas_valor_decrescente(self):
-        pass
-    def ordem_despesas_ordem_alfabética(self):
-        pass
+        self.controller.escrever_ficheiro_json('data.json',self.controller.data)
+       
